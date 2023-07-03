f30206eef2899e6ca5706395dee267c9c7549459
Ajay <52690028+ajaykasturi@users.noreply.github.com>
2023-07-03 23:08:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 import OPENAI_API_KEY from "./key.js";
 
+
+let result="";
+let speed = 10;
+function scrollToBottom() {
+    document.getElementById("responseid").scrollTop =document.getElementById("responseid").scrollHeight;
+}
+function typeWriter(i) {
+    if (i < result.length) {
+        scrollToBottom();
+        document.querySelector(".response").value += result.charAt(i);
+        //console.log(result.charAt(i));
+        i++;
+        setTimeout(typeWriter, speed,i);
+    }
+
+}
 document.querySelector("i").addEventListener("click",function(event){
     letsGo();
 });
 document.querySelector(".prompt").addEventListener("keydown",function(event){
-    if(event.key.toLowerCase()=='enter'){
+    if(event.key.toLowerCase()==='enter'){
         letsGo();
     }else{
         console.log("something went wrong");
@@ -13,9 +29,12 @@ document.querySelector(".prompt").addEventListener("keydown",function(event){
 function letsGo(){
     let quest = document.querySelector(".prompt").value;
     document.querySelector(".response").value="";
+    // document.getElementById("responseid").addEventListener("input", function(){textarea.scrollTop = textarea.scrollHeight;});
     runCompletion(quest);
-    document.querySelector(".prompt").value="";
+    document.getElementById("prompt").setSelectionRange(0, 0);
+    document.querySelector(".prompt").value='';
 }
+
 async function runCompletion(question){
     try{
         const response = await fetch('https://api.openai.com/v1/completions',{
@@ -28,22 +47,27 @@ async function runCompletion(question){
             body: JSON.stringify({
                 'model': "text-davinci-003",
                 'prompt': question,
-                'max_tokens': 500,
+                'max_tokens': 1000,
                 'temperature': 0.2
             }),
         });
         if(response.ok){
             const data = await response.json();
-            document.querySelector(".response").value=(data.choices[0].text).trim();
+            //document.querySelector(".response").value
+            result = (data.choices[0].text.trim());
+            //console.log(result);
             //console.log(data.choices[0].text);
+             
         } else{
-            document.querySelector(".response").value="Unable to process your request";
+            document.querySelector(".response").value="Unable to process your request1";
             //console.log("fail");
         }
     }
     catch(error){
-        document.querySelector(".response").value="Unable to process your request";
+        document.querySelector(".response").value="Unable to process your request2";
         //console.log(error);
         //change the response area unable to process your request
     }
+    // typeWriter(result);
+    typeWriter(0);
 }
